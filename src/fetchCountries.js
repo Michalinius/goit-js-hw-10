2b6b2b59556641cde2578dcd4105119e37c91080
Michalinius <michalinius@gmail.com>
2022-09-04 13:56:02 +0200

    done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,23 +1,24 @@
 export function fetchCountries(name) {
-return fetch(`https://restcountries.com/v3.1/name/${name.trim()}?fields=name,capital,population,flags,languages`)
-  .then(response => response.json())
-  .then(data => {
-    if(data?.length){
+  return fetch(`https://restcountries.com/v3.1/name/${name.trim()}?fields=name,capital,population,flags,languages`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.length) {
         const newData = data.map(country => {
-            return {
-                population : country.population,
-                capital : country.capital[0],
-                languages : country.languages,
-                name : country.name.official,
-                flag : country.flags.svg
-            }
+          return {
+            population: country.population,
+            capital: country.capital[0],
+            languages: country.languages,
+            name: country.name.official,
+            flag: country.flags.svg
+          }
         })
-    return newData
-    }
-    
-    return []
-  })
-  .catch(error => {
-    console.warn(error)
-});
+        console.log(newData)
+        return newData
+      }
+
+      return []
+    })
+    .catch(error => {
+      console.warn(error)
+    });
 }
--- src/index.js
+++ src/index.js
@@ -6,22 +6,43 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector("#search-box");
+const listOfCountries = document.querySelector(".country-list")
+const oneCountry = document.querySelector(".country-info")
 
-input.addEventListener("input", debounce(event =>{
+const listLoop = (data) => {
+    let array = [];
+    for (i = 0; i < data.length; i++) {
+        array.push(`<li style="display:flex; list-style:none; align-items:baseline;"><img style="margin-right:20px;" src="${data[i].flag}" alt="There's should be a flag" width="20px" height="10px"> <p>${data[i].name}</p></li>`);
+    }
+    array = array.join("");
+    console.log(array);
+    return array;
+}
+
+
+
+input.addEventListener("input", debounce(event => {
     //tu czyścic
-    if(event.target.value === ""){
+    if (event.target.value === "") {
+        listOfCountries.innerHTML = "";
+        oneCountry.innerHTML = "";
         return
     }
 
     fetchCountries(event.target.value).then(data => {
-    if (data.length>10) 
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-    else if (data.length==0) 
-        Notiflix.Notify.failure("Oops, there is no country with that name")
-    else if (data.length==1)
-        console.log('asd')
-    else if (data.length >= 2 && data.length <= 10)
-        console.log('asdd')
+        if (data.length > 10)
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+        else if (data.length == 0) {
+            Notiflix.Notify.failure("Oops, there is no country with that name");
+        }
+        else if (data.length == 1)
+            oneCountry.innerHTML = (`<span style="display:flex; align-items:center; justify-content:center;"><img style="margin-right:20px;" src="${data[0].flag}" alt="There's should be a flag" width="500px" height="100%"> <p style="font-size:42px; font-weight:700;">${data[0].name}</p></span><span style="display:flex; align-items:center; justify-content:center; flex-direction:column; font-size:20px;"><p>Capital: ${data[0].capital}</p><p>Population: ${data[0].population}</p><p>Languages: ${Object.values(data[0].languages)}</p></span>`);
+        else if (data.length >= 2 && data.length <= 10) {
+            listLoop(data)
+            console.log('asdd')
+            listOfCountries.innerHTML = listLoop(data);
+            console.log(listOfCountries)
+        }
     })
-    
-},DEBOUNCE_DELAY))
+
+}, DEBOUNCE_DELAY))
