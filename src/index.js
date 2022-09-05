18c64b411bc289f5632fd12f657de0e2bc267943
Michalinius <michalinius@gmail.com>
2022-09-05 21:21:12 +0200

    done

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const oneCountry = document.querySelector(".country-info")
 
 const listLoop = (data) => {
     let array = [];
-    for (i = 0; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
         array.push(`<li style="display:flex; list-style:none; align-items:baseline;"><img style="margin-right:20px;" src="${data[i].flag}" alt="There's should be a flag" width="20px" height="10px"> <p>${data[i].name}</p></li>`);
     }
     array = array.join("");
@@ -34,6 +34,8 @@ input.addEventListener("input", debounce(event => {
             Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
         else if (data.length == 0) {
             Notiflix.Notify.failure("Oops, there is no country with that name");
+            listOfCountries.innerHTML = "";
+            oneCountry.innerHTML = "";
         }
         else if (data.length == 1)
             oneCountry.innerHTML = (`<span style="display:flex; align-items:center; justify-content:center;"><img style="margin-right:20px;" src="${data[0].flag}" alt="There's should be a flag" width="500px" height="100%"> <p style="font-size:42px; font-weight:700;">${data[0].name}</p></span><span style="display:flex; align-items:center; justify-content:center; flex-direction:column; font-size:20px;"><p>Capital: ${data[0].capital}</p><p>Population: ${data[0].population}</p><p>Languages: ${Object.values(data[0].languages)}</p></span>`);
